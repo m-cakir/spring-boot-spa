da287abdeb414b4f1c18d1c7ed877ba627ebc315
mcakir <mcakir.web@gmail.com>
2018-07-11 16:19:04 +0300

    spring boot

---
+++ src/main/java/com/mcakir/SpringBootSpaApplication.java
@@ -0,0 +1,12 @@
+package com.mcakir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpaApplication.class, args);
+	}
+}
